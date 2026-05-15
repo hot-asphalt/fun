4b9953ee00b95530c54eed7308d0da79faaae0a5
Billy Bund <billybund@gmail.com>
2026-05-15 10:01:49 -0400

    lint issues

--- raw_a.py
+++ raw_a.py
@@ -1,9 +1,8 @@
 from seleniumbase import SB
-import os
 
 with SB(uc=True, test=True, guest=True) as sb:
     # url = os.environ["URL"]
-    url = "https://www.aa.com/booking/search?locale=en_US&fareType=Lowest&pax=3&adult=3&type=OneWay&searchType=Award&cabin=&carriers=ALL&travelType=personal&slices=%5B%7B%22orig%22:%22CLT%22,%22origNearby%22:false,%22dest%22:%22HPN%22,%22destNearby%22:false,%22date%22:%222026-06-26%22%7D%5D" # noqa: E501
+    url = "https://www.aa.com/booking/search?locale=en_US&fareType=Lowest&pax=3&adult=3&type=OneWay&searchType=Award&cabin=&carriers=ALL&travelType=personal&slices=%5B%7B%22orig%22:%22CLT%22,%22origNearby%22:false,%22dest%22:%22HPN%22,%22destNearby%22:false,%22date%22:%222026-06-26%22%7D%5D"  # noqa: E501
     sb.activate_cdp_mode(url)
     print("Sleeping for 30 seconds")
     title = sb.get_title()
