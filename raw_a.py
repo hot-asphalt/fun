aa5e2ab12f32acd9ffd7c98797b2cf69ff4e5b0d
Billy Bund <billybund@gmail.com>
2026-05-15 09:48:05 -0400

    more logging

--- raw_a.py
+++ raw_a.py
@@ -5,8 +5,12 @@ with SB(uc=True, test=True, guest=True) as sb:
     url = os.environ["URL"]
     sb.activate_cdp_mode(url)
     print("Sleeping for 30 seconds")
+    title = sb.get_title()
+    print("Title: ", title)
     sb.sleep(30)
     rows = sb.find_elements("div.flight-row")
+    title = sb.get_title()
+    print("Title: ", title)
     print(f"Found {len(rows)} rows")
 
     for row in rows:
