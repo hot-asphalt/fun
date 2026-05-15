18d5f6d1ade5630b1774652093e5604f07ca83c8
Billy Bund <billybund@gmail.com>
2026-05-15 09:55:25 -0400

    random test

--- raw_a.py
+++ raw_a.py
@@ -3,7 +3,7 @@ import os
 
 with SB(uc=True, test=True, guest=True) as sb:
     url = os.environ["URL"]
-    sb.activate_cdp_mode(url)
+    sb.activate_cdp_mode("https://www.aa.com/booking/search?locale=en_US&fareType=Lowest&pax=3&adult=3&type=OneWay&searchType=Award&cabin=&carriers=ALL&travelType=personal&slices=%5B%7B%22orig%22:%22CLT%22,%22origNearby%22:false,%22dest%22:%22HPN%22,%22destNearby%22:false,%22date%22:%222026-06-26%22%7D%5D")
     print("Sleeping for 30 seconds")
     title = sb.get_title()
     print("Title: ", title)
